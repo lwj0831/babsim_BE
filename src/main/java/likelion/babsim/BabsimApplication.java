f8b8a489279ff61c1942e3dd0afe8ca63638cc63
lwj0831 <wjl0831@gmail.com>
2024-07-17 20:09:27 +0900

    initial commit

---
+++ src/main/java/likelion/babsim/BabsimApplication.java
@@ -0,0 +1,13 @@
+package likelion.babsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabsimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion/babsim/BabsimApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.babsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabsimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
