9c1d86a3cbc316da0aa53dc2de125319cc26bc42
lwj0831 <wjl0831@gmail.com>
2024-07-24 20:04:12 +0900

    chore: Change delimiter , to / only in recipeContents

--- src/main/java/likelion/babsim/domain/formatter/RecipeDetailImgFormatter.java
+++ src/main/java/likelion/babsim/domain/formatter/RecipeDetailImgFormatter.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class RecipeDetailImgFormatter {
     public static List<String> parseRecipeDetailImgList(String urlsStr) {
         List<String> urlList = new ArrayList<>();
-        String[] urls = urlsStr.split("/"); // 문자열을 쉼표로 분리하여 각 URL을 가져옴
+        String[] urls = urlsStr.split(","); // 문자열을 쉼표로 분리하여 각 URL을 가져옴
         for (String url : urls) {
             url = url.trim(); // 공백 제거
             if (!url.isEmpty()) {
--- src/main/java/likelion/babsim/domain/formatter/RecipeTimerFormatter.java
+++ src/main/java/likelion/babsim/domain/formatter/RecipeTimerFormatter.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class RecipeTimerFormatter {
     public static List<Integer> parseTimerList(String timersStr) {
         List<Integer> timerList = new ArrayList<>();
-        String[] timers = timersStr.split("/"); // 문자열을 쉼표로 분리하여 각 timer을 가져옴
+        String[] timers = timersStr.split(","); // 문자열을 쉼표로 분리하여 각 timer을 가져옴
         for (String timer : timers) {
             timer = timer.trim(); // 공백 제거
             if (!timer.isEmpty()) {
